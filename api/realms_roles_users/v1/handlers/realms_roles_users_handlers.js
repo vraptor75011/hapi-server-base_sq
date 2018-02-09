848cd1e1d8f02f0571055741d9543314f9225982
Giacomo PIazzesi <giapiazze@gmail.com>
2018-02-09 16:27:22 +0100

    20180209 - Models Handlers. Added Log for request

--- api/realms_roles_users/v1/handlers/realms_roles_users_handlers.js
+++ api/realms_roles_users/v1/handlers/realms_roles_users_handlers.js
@@ -1,5 +1,6 @@
 const DB = require('../../../../config/sequelize');
 const HandlerHelper = require('../../../../utilities/handler/handler-helper');
+const Log = require('../../../../utilities/logging/logging');
 
 const RealmsRolesUsers = DB.RealmsRolesUsers;
 
@@ -9,6 +10,7 @@ const Handler =
 		findAll: async (request, h) => {
 			// Call listAll async function with await inside handler-helper
 			// call LIST Handler for CRUD function valid for all present models
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			let result = await HandlerHelper.list(RealmsRolesUsers, request.query);
 			return result;
 
@@ -17,6 +19,7 @@ const Handler =
 		findOne: async (request, h) => {
 			// Call an async function with await inside in handler-helper
 			// call FIND ONE Handler for CRUD function valid for all present models
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			let result = await HandlerHelper.find(RealmsRolesUsers, request.params.realmsRolesUsersId, request.query);
 			return result;
 
@@ -25,7 +28,7 @@ const Handler =
 		create: async (request, h) => {
 			// Call an async function with await inside in handler-helper
 			// Only for Admin to Add a new RealmsRolesUsers without free registration
-
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			// call CREATE Handler for CRUD function valid for all present models
 			let result = await HandlerHelper.create(RealmsRolesUsers, request.payload);
 			return result;
@@ -35,7 +38,7 @@ const Handler =
 		update: async (request, h) => {
 			// Admin and RealmsRolesUsers can update an RealmsRolesUsers, but RealmsRolesUsers can't change his roles and realms
 			// Call an async function with await inside in handler-helper
-
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			// call CREATE Handler for CRUD function valid for all present models
 			let result = await HandlerHelper.update(RealmsRolesUsers, request.params.realmsRolesUsersId, request.payload);
 			return result;
@@ -44,7 +47,7 @@ const Handler =
 		delete: async (request, h) => {
 			// Admin can delete an RealmsRolesUsers
 			// Call an async function with await inside in handler-helper
-
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			// call DELETE Handler for CRUD function valid for all present models
 			let result = await HandlerHelper.deleteOne(RealmsRolesUsers, request.params.realmsRolesUsersId, request.payload);
 					return result;
@@ -53,7 +56,7 @@ const Handler =
 		deleteMany: async (request, h) => {
 			// Admin can delete an RealmsRolesUsers
 			// Call an async function with await inside in handler-helper
-
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			// call DELETE MANY Handler for EXTRA CRUD function valid for all present models
 			let result = await HandlerHelper.deleteMany(RealmsRolesUsers, request.payload);
 			return result;
@@ -62,6 +65,7 @@ const Handler =
 		addOne: async (request, h) => {
 			// Admin can add one child model to an RealmsRolesUsers
 			// Call an async function with await inside in handler-helper
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			let childModel = RealmsRolesUsers.associations[request.params.childModel].target;
 			// call ADD_ONE Handler for EXTRA CRUD function valid for all present models and a new child model
 			let result = await HandlerHelper.addOne(RealmsRolesUsers, request.params.realmsRolesUsersId, childModel, request.params.childId, request.params.childModel);
@@ -71,6 +75,7 @@ const Handler =
 		removeOne: async (request, h) => {
 			// Admin can remove one child model from an RealmsRolesUsers
 			// Call an async function with await inside in handler-helper
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			let childModel = RealmsRolesUsers.associations[request.params.childModel].target;
 			// call REMOVE_ONE Handler for EXTRA CRUD function valid for all present models and a new child model
 			let result = await HandlerHelper.removeOne(RealmsRolesUsers, request.params.realmsRolesUsersId, childModel, request.params.childId, request.params.childModel);
@@ -80,6 +85,7 @@ const Handler =
 		addMany: async (request, h) => {
 			// Admin can add one or more child model to an RealmsRolesUsers
 			// Call an async function with await inside in handler-helper
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			let childModel = RealmsRolesUsers.associations[request.params.childModel].target;
 			// call ADD_MANY Handler for EXTRA CRUD function valid for all present models and a new child model
 			let result = await HandlerHelper.addMany(RealmsRolesUsers, request.params.realmsRolesUsersId, childModel, request.params.childModel, request.payload);
@@ -89,6 +95,7 @@ const Handler =
 		removeMany: async (request, h) => {
 			// Admin can remove one or more child model from an RealmsRolesUsers
 			// Call an async function with await inside in handler-helper
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			let childModel = RealmsRolesUsers.associations[request.params.childModel].target;
 			// call REMOVE_MANY Handler for EXTRA CRUD function valid for all present models and a new child model
 			let result = await HandlerHelper.removeMany(RealmsRolesUsers, request.params.realmsRolesUsersId, childModel, request.params.childModel, request.payload);
@@ -98,6 +105,7 @@ const Handler =
 		getAll: async (request, h) => {
 			// Admin can get list of Child model related to RealmsRolesUsers
 			// Call an async function with await inside in handler-helper
+			Log.apiLogger.info('Method: ' + request.method.toUpperCase() + ' Request: ' + request.path);
 			let childModel = RealmsRolesUsers.associations[request.params.childModel].target;
 			// call GET_ALL Handler for EXTRA CRUD function valid for all present models and a new child model
 			let result = await HandlerHelper.getAll(RealmsRolesUsers, request.params.realmsRolesUsersId, childModel, request.params.childModel, request.query);
